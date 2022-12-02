ba97553fd2488971cacb066fe42ef4a376c8cc88
maxicapodacqua <maxi.capodacqua@gartner.com>
2022-12-02 10:18:15 -0600

    Done part 2

---
+++ 2022/02/part2/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+var shapeScore = map[string]int{
+	"A": 1, // rock
+	"B": 2, // paper
+	"C": 3, //scissors
+}
+
+var outcomeMap = map[string]int{
+	"X": 0, // lose
+	"Y": 3, // draw
+	"Z": 6, //win
+}
+
+var stdShapes = map[string]string{
+	"A": "R", // rock
+	"B": "P", // paper
+	"C": "S", // scissors
+	"X": "R",
+	"Y": "P",
+	"Z": "S",
+}
+
+var winningMap = map[string]string{
+	"A": "B", // rock < paper
+	"B": "C", // paper < scissors
+	"C": "A", // scissors < rock
+}
+
+// invert of losing map
+var losingMap = map[string]string{
+	"B": "A",
+	"C": "B",
+	"A": "C",
+}
+
+// rock > scissors
+
+// paper > rock
+
+// scissors > paper
+
+func guessShape(theirs, ours string) int {
+	// draw returns same shape as theirs
+	if ours == "Y" {
+		return shapeScore[theirs]
+	}
+
+	// loss
+	if ours == "X" {
+		return shapeScore[losingMap[theirs]]
+	}
+
+	return shapeScore[winningMap[theirs]]
+}
+
+func calculateScore(game []string) int {
+	//score := 0
+
+	theirs, ours := game[0], game[1]
+	outcomeRound := outcomeMap[ours]
+	ourScore := guessShape(theirs, ours)
+	//outcomeRound := calculateOutcome(theirs, ours)
+
+	fmt.Printf("calculate score, game: %v ourScore: %v, outcome: %v\n", game, ourScore, outcomeRound)
+
+	return ourScore + outcomeRound
+}
+func main() {
+	f, err := os.Open("./2022/02/input.txt")
+	//f, err := os.Open("input_test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	csvR := csv.NewReader(f)
+	csvR.Comma = ' '
+	games, err := csvR.ReadAll()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("games: %v \n", games)
+	var scores []int
+
+	acc := 0
+	for _, game := range games {
+		score := calculateScore(game)
+		scores = append(scores, score)
+		acc += score
+		//fmt.Printf("%v\n", game)
+		//scores[i] = 11
+	}
+
+	fmt.Printf("scores: %v\n", scores)
+	fmt.Printf("acc total score: %v\n", acc)
+
+}
