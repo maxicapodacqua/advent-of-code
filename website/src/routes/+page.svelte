f5e064984eb97de545a653466f95b80f7a36c590
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-12-01 22:54:08 -0600

    Adding sveltekit website

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { process } from "day-01";
+
+  console.log(
+    process(`1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`)
+  );
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
