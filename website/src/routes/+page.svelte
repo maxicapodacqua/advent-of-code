5ad3f2aa61d0cc557c49319b209937ce7662cc7a
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-12-04 12:12:02 -0600

    Generating two functions

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-  import { process } from "day-01";
+  import { processPart1 } from "day-01";
 
   console.log(
-    process(`1abc2
+    processPart1(`1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`)
