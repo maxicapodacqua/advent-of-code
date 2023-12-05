d180e5ccd0b502fdddf2104e7147065bfe9c1767
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-12-05 14:29:02 -0600

    VEEERY slow solution, but worked

--- 2023/rust/day-05/src/part2.rs
+++ 2023/rust/day-05/src/part2.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{cmp, str::FromStr};
 
 #[derive(Debug)]
 struct PlantSource {
@@ -50,9 +50,42 @@ impl FromStr for PlantSource {
         Ok(PlantSource { modifiers })
     }
 }
+// Parsed seed line by creating tuples that represent (start, end) of an
+// inclusive Range start..=range.
+// Sorts the tuples by start value and then proceeds to merge intervals to avoid
+// parsing the same seed twice
+fn parse_seed_line(input: &str) -> Vec<(usize, usize)> {
+    let seed_iter = input
+        .split_whitespace()
+        .skip(1)
+        .map(|val| val.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let seed_by_2 = seed_iter.chunks(2);
+    // dbg!(&seed_by_2.collect::<Vec<_>>());
+    let mut sorted_seeds: Vec<(usize, usize)> = vec![];
+    for seed_data in seed_by_2 {
+        sorted_seeds.push((seed_data[0], (seed_data[0] + seed_data[1] - 1)));
+    }
+    sorted_seeds.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut joined_seeds: Vec<(usize, usize)> = vec![];
+    joined_seeds.push(sorted_seeds[0].clone());
+    for i in 1..sorted_seeds.len() {
+        let curr = sorted_seeds[i].clone();
+        let j = joined_seeds.len() - 1;
+
+        if curr.0 >= joined_seeds[j].0 && curr.0 <= joined_seeds[j].1 {
+            joined_seeds[j].1 = cmp::max(curr.1, joined_seeds[j].1);
+        } else {
+            joined_seeds.push(curr);
+        }
+    }
+    joined_seeds
+}
 
 pub fn process(input: &str) -> String {
     let seeds_line = input.lines().next().unwrap();
+    let parsed_seed_line = parse_seed_line(&seeds_line);
 
     let mut body = input
         .split("\n\n")
@@ -68,20 +101,18 @@ pub fn process(input: &str) -> String {
 
     let mut min_location = usize::MAX;
 
-    for seed in seeds_line
-        .split_whitespace()
-        .skip(1)
-        .map(|seed| seed.parse::<usize>().unwrap())
-    {
-        let s = soil.get_source(seed);
-        let f = fertilizer.get_source(s);
-        let w = water.get_source(f);
-        let l = light.get_source(w);
-        let t = temperature.get_source(l);
-        let h = humidity.get_source(t);
-        let loc = location.get_source(h);
+    for (start, end) in parsed_seed_line.iter() {
+        for seed in *start..=*end {
+            let s = soil.get_source(seed);
+            let f = fertilizer.get_source(s);
+            let w = water.get_source(f);
+            let l = light.get_source(w);
+            let t = temperature.get_source(l);
+            let h = humidity.get_source(t);
+            let loc = location.get_source(h);
 
-        min_location = min_location.min(loc);
+            min_location = min_location.min(loc);
+        }
     }
 
     min_location.to_string()
@@ -91,6 +122,23 @@ mod tests {
 
     use crate::part2::{process, PlantSource};
 
+    use super::parse_seed_line;
+
+    #[test]
+    fn parse_seed_line_works() {
+        let input = r#"seeds: 79 14 55 13"#;
+        let res = parse_seed_line(input);
+        assert_eq!(res.len(), 2);
+        assert_eq!(res[0], (55, 67));
+        assert_eq!(res[1], (79, 92));
+
+        let input = r#"seeds: 79 14 55 33"#;
+        let res = parse_seed_line(input);
+        assert_eq!(res.len(), 1);
+        assert_eq!(res[0], (55, 92));
+
+        // dbg!(res);
+    }
     #[test]
     fn plant_from_str_works() {
         let input = r#"seed-to-soil map:
