cebedc496a4bd4539088cb69134e6f13e6c968d0
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-12-01 16:45:22 -0600

    Initial import for elixir attempt

---
+++ 2024/elixir/aoc2024/lib/aoc/day01.ex
@@ -0,0 +1,10 @@
+# create basic structure for day 1
+defmodule AOC.Day01 do
+  def part1 do
+    IO.puts "Part 1"
+  end
+
+  def part2 do
+    IO.puts "Part 2"
+  end
+end
---
+++ 2024/elixir/aoc2024/test/aoc/day01.exs
@@ -0,0 +1,13 @@
+# basic test file
+defmodule AOC.Day01Test do
+  use ExUnit.Case
+  doctest AOC.Day01
+
+  test "part1" do
+    assert true
+  end
+
+  test "part2" do
+    assert true
+  end
+end
