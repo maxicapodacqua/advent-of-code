4e08f19c6d266579fcb6d6c96b5ae02e92d3f3a4
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-12-03 16:20:12 -0600

    I needed help for part 2

--- 2024/elixir/aoc2024/lib/aoc/day03.ex
+++ 2024/elixir/aoc2024/lib/aoc/day03.ex
@@ -3,18 +3,40 @@ defmodule Aoc2024.Day03 do
     Regex.scan(~r/mul\((?<a>\d{1,3})\,(?<b>\d{1,3})\)/, input, [])
     |> Enum.map(fn matchData ->
       matchTuple = List.to_tuple(matchData)
-      a = String.to_integer(elem(matchTuple,1))
-      b = String.to_integer(elem(matchTuple,2))
+      a = String.to_integer(elem(matchTuple, 1))
+      b = String.to_integer(elem(matchTuple, 2))
       a * b
-     end)
+    end)
     |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    # Capture all operations, mul, do and don't
+    Regex.scan(~r/mul\((\d{1,3})\,(\d{1,3})\)|do\(\)|don\'t\(\)/, input)
+    # very interesting way of using reduce
+    |> Enum.reduce({:continue, 0}, fn
+      # pattern mathing the parameters used in the reduce callback
+      ["don't()"], {_, acc} ->
+        {:skip, acc}
+
+      ["do()"], {_, acc} ->
+        {:continue, acc}
+
+      [_, _, _], {:skip, acc} ->
+        {:skip, acc}
+
+      [_, a, b], {:continue, acc} ->
+        {:continue, acc + String.to_integer(a) * String.to_integer(b)}
+    end)
+    |> elem(1)
   end
 
   def runPart1() do
     {:ok, content} = File.read("#{__DIR__}/input_day03.txt")
     part1(content)
+  end
+  def runPart2() do
+    {:ok, content} = File.read("#{__DIR__}/input_day03.txt")
+    part2(content)
   end
 end
--- 2024/elixir/aoc2024/test/aoc/day03_test.exs
+++ 2024/elixir/aoc2024/test/aoc/day03_test.exs
@@ -9,5 +9,8 @@ defmodule Aoc2024.Day03Test do
   end
 
   test "part2" do
+    # input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assert part2(input) == 48
   end
 end
